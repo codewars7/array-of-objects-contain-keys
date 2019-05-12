3f11f2042c6351b3c228fb02d9d9d0dc69a87f2e
americanman <snowboardracer7@gmail.com>
2019-05-12 14:11:20 -0400

    Add comments

--- index.js
+++ index.js
@@ -8,11 +8,13 @@
 // seconde approach with every method
 
 const truthCheck = (collection, pre) => {
+// every checks if value returned from the callback is truthy
   return collection.every(item => item[pre])
 }
 
+// should return true
 console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"},
-                        {"user": "Dipsy", "sexy": "male"},
+                        {"user": "Dipsy", "sex": "male"},
                         {"user": "Laa-Laa", "sex": "female"},
                         {"user": "Po", "sex": "female"}],
                         "sex"))
