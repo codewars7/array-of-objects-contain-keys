824542783122ed408b25832b96229dc5f7defb1b
americanman <snowboardracer7@gmail.com>
2019-05-12 14:09:02 -0400

    Add second approach

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-const filteredKeys = (collection, pre) => collection.filter(item => item[pre]).length
-const originalKeys = (collection, pre) => collection.map(item => item).length
+// const filteredKeys = (collection, pre) => collection.filter(item => item[pre]).length
+// const originalKeys = (collection, pre) => collection.map(item => item).length
+
+// const truthCheck = (collection, pre) => {
+//  return filteredKeys(collection, pre) === originalKeys(collection, pre) ? true : false
+// }
+
+// seconde approach with every method
 
 const truthCheck = (collection, pre) => {
- return filteredKeys(collection, pre) === originalKeys(collection, pre) ? true : false
+  return collection.every(item => item[pre])
 }
 
 console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"},
-                        {"user": "Dipsy", "sex": "male"},
+                        {"user": "Dipsy", "sexy": "male"},
                         {"user": "Laa-Laa", "sex": "female"},
                         {"user": "Po", "sex": "female"}],
                         "sex"))
